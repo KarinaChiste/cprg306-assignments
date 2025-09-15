5290cca9120b2a2ec496021207978141cb879bc0
KarinaChiste <163331375+KarinaChiste@users.noreply.github.com>
2025-09-15 13:24:33 -0600

    week 2 page completed

---
+++ app/week-2/student-info.js
@@ -0,0 +1,10 @@
+import Link from "next/link";
+export function StudentInfo(){
+  return (
+    <div>
+      <h1>Name: Karina Chiste</h1>
+      GitHub: <Link href= "https://github.com/KarinaChiste/cprg306-assignments">KarinaChiste/cprg306-assignments</Link>
+    </div>
+  
+  );
+}
