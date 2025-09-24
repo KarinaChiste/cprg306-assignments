073d83314f70df5b25d4f5897cd5c8dba8aa78a6
KarinaChiste <163331375+KarinaChiste@users.noreply.github.com>
2025-09-24 17:56:40 -0600

    Styling added to week 3

--- app/week-3/item.js
+++ app/week-3/item.js
@@ -1,9 +1,9 @@
 export function Item(props){
   return(
-    <ul>
-      <li>{props.name}</li>
-      <li>Quantity: {props.quantity}</li>
-      <li>Category: {props.category}</li>
+    <ul className="my-3 border-1 p-7 mx-150 rounded-4xl">
+      <li className="text-center">{props.name}</li>
+      <li className="text-center">Quantity: {props.quantity}</li>
+      <li className="text-center">Category: {props.category}</li>
     </ul>
   )
 }
--- app/week-3/page.js
+++ app/week-3/page.js
@@ -2,7 +2,7 @@ import {ItemList} from "./item-list.js"
 export default function Page(){
   return(
     <main>
-      <h1>Shopping List</h1>
+      <h1 className="font-bold text-3xl text-center p-2">Shopping List</h1>
       <ItemList/>
     </main>
   )
