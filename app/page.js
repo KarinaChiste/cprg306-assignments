45372245a3d9c1b0edf9d6a2f4d23002faf4fa0a
KarinaChiste <163331375+KarinaChiste@users.noreply.github.com>
2025-10-09 17:19:04 -0600

    week 5 functionality implemented

--- app/page.js
+++ app/page.js
@@ -6,6 +6,7 @@ export default function Page() {
       <Link href="/week-2"> Go To Week 2</Link>
       <Link href="/week-3"><br/>Go To Week 3</Link>
       <Link href="/week-4"><br/>Go To Week 4</Link>
+      <Link href="/week-5"><br/>Go To Week 5</Link>
     </main>
   );
 }
