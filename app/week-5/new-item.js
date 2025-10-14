bf2d0ab8985ff7fd3ef85623249974271f913e00
KarinaChiste <163331375+KarinaChiste@users.noreply.github.com>
2025-10-14 07:57:00 -0600

    styling added to week 5

--- app/week-5/new-item.js
+++ app/week-5/new-item.js
@@ -68,41 +68,48 @@ export function NewItem()
   return(
     <main>
       <h2 className="font-bold text-2xl text-center">Week 5 - New Item</h2>
-      <div className="border-1 mx-30 w-100 justify-self-center rounded-2xl">
+      <div className="border-1 mx-30 w-100 justify-self-center rounded-2xl p-2">
         <form onSubmit={handleSubmit}>
-          <input type="text" onChange={handleNameChange} value={name}  required/>
-        <div>
-          <p className="text-center">Current:<span className="font-bold text-2xl">{quantity}</span></p>
-          <div className="justify-self-center">
-            <button onClick={increment}
-            disabled={disabledIncrement}
-            type="button"
-            className="border-1 my-1 py-3 px-5 mx-3 bg-blue-500 text-white rounded-2xl disabled:bg-blue-300">
-              +
-            </button>
-            
-            <button onClick={decrement}
-            disabled={disabledDecrement}
-            type="button"
-            className="border-1 my-1 mx-3 px-5 py-3 rounded-2xl bg-gray-100 disabled:bg-gray-300">
-              -
-            </button>
+          <label className="text-sm">Item Name:</label>
+          <br/>
+          <input type="text" onChange={handleNameChange} value={name}  required className="border-1 mx-1"/>
+          <br/>
+          <label className="text-sm">Quantity:</label>
+          <div>
+            <p>Current:<span className="font-bold text-2xl">{quantity}</span></p>
+            <div>
+              <button onClick={increment}
+              disabled={disabledIncrement}
+              type="button"
+              className="border-1 my-1 py-3 px-5 mx-3 bg-blue-500 text-white rounded-2xl disabled:bg-blue-300">
+                +
+              </button>
+              
+              <button onClick={decrement}
+              disabled={disabledDecrement}
+              type="button"
+              className="border-1 my-1 mx-3 px-5 py-3 rounded-2xl bg-gray-100 disabled:bg-gray-300">
+                -
+              </button>
+            </div>
           </div>
-        </div>
-        <select onChange={handleCategoryChange} value={category}>
-          <option value={"produce"}>Produce</option>
-          <option value={"dairy"}>Dairy</option>
-          <option value={"bakery"}>Bakery</option>
-          <option value={"meat"}>Meat</option>
-          <option value={"frozen foods"}>Frozen Foods</option>
-          <option value={"canned goods"}>Canned Goods</option>
-          <option value={"dry goods"}>Dry Goods</option>
-          <option value={"beverages"}>Beverages</option>
-          <option value={"snacks"}>Snacks</option>
-          <option value={"household"}>Household</option>
-          <option value={"other"}>Other</option>
-        </select>
-        <button type="submit">Submit</button>
+          <label className="text-sm">Category:</label>
+          <br/>
+          <select onChange={handleCategoryChange} value={category}>
+            <option value={"produce"}>Produce</option>
+            <option value={"dairy"}>Dairy</option>
+            <option value={"bakery"}>Bakery</option>
+            <option value={"meat"}>Meat</option>
+            <option value={"frozen foods"}>Frozen Foods</option>
+            <option value={"canned goods"}>Canned Goods</option>
+            <option value={"dry goods"}>Dry Goods</option>
+            <option value={"beverages"}>Beverages</option>
+            <option value={"snacks"}>Snacks</option>
+            <option value={"household"}>Household</option>
+            <option value={"other"}>Other</option>
+          </select>
+          <br/>
+          <button type="submit" className="border-1 bg-green-600 text-white p-3 m-2 rounded-2xl">Add Item</button>
         </form>
         
         
